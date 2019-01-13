c5ab2f55db3fa157103ac58b66cc0b652dcee190
Aneta Prokopiuk <anetaprokopiuk@gmail.com>
2019-01-13 16:17:40 +0100

    deck T

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,5 +1,23 @@
-public class Card {
+abstract class Card {
     CardSuit cardSuit;
     int faceValue;
 
+    Card(CardSuit suit, int value){
+        this.cardSuit = suit;
+        this.faceValue = value;
+    }
 }
+
+class FourColorCard extends Card{
+    private boolean _isRed;
+
+    FourColorCard(CardSuit suit, int value){
+        super(suit, value);
+        this._isRed = (this.cardSuit == CardSuit.DIAMOND || this.cardSuit == CardSuit.HEART);
+    }
+
+    boolean isRed(){
+        return this._isRed;
+    }
+}
+
--- src/main/java/FullDeck.java
+++ src/main/java/FullDeck.java
@@ -1,7 +1,20 @@
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
-public class FullDeck<T extends Card>{
-    private List <T> Cards;
-    
+public class FullDeck<T extends Card> {
 
+    int cardSuit;
+    Enum faceValue;
+
+    private List<T> cards = new ArrayList<>();
+
+    public FullDeck() {
+        for(CardSuit suit : EnumSet.allOf(CardSuit.class)) {
+            for (int rank = 2; rank < 15; rank++) {
+                cards.add(new T(suit, rank));
+            }
+        }
+    }
 }
